3e301422e16def347cf618381d6919cf4acedf7c
Mohit Agrawal <mohit.agrawal@avizva.com>
2020-08-16 09:45:00 +0530

    css fixes

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import Head from 'next/head'
 import { home as HomeComponent } from '../frontend/js/components/home'
 import { header as HeaderComponent } from '../frontend/js/components/header'
+import { Fragment } from 'react'
 
 export default function Home() {
   
   return (
-    <div >
+    <Fragment >
       <Head>
         <title>Welcome to e-Commerce</title>
       </Head>
       <HeaderComponent />
-      <HomeComponent />
-    </div>
+      <div className = 'page-with-header' >
+        <HomeComponent />
+      </div>
+    </Fragment>
   )
 }
