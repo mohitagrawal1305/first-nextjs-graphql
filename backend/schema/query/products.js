a169dc6c05d660034e249fdc92f23b6c58aa63f0
Mohit Agrawal <mohit.agrawal@avizva.com>
2020-08-26 00:19:05 +0530

    updated products api to filter based on query

--- backend/schema/query/products.js
+++ backend/schema/query/products.js
@@ -1,15 +1,33 @@
+const graphql = require( 'graphql' );
+const { isEmpty } = require( 'lodash' );
+
 const { ProductType } = require( '../types/product' );
 const Product = require( '../../models/Product' );
-const graphql = require( 'graphql' );
 
 const {
     GraphQLList,
+    GraphQLString
 } = graphql;
 
 module.exports = {
     type: new GraphQLList( ProductType ),
+    args: { query: { type: GraphQLString } },
     async resolve( parent, args ) {
+
+        const { query } = args;
+
+        
         const products = await Product.find();
+        
+        if( !isEmpty( query ) ) {
+            const _products = products.filter( item => {
+
+                return !isEmpty( item.name.toLowerCase().replace( ' ', '' ).match( query.toLowerCase().replace( ' ', '' ) ) );
+            } );
+
+            return _products;
+        }
+
         return products;
     }
 };
