ec7507e7c2bdde5d9748c6f59d5e57e4cd59559a
Mohit Agrawal <mohit.agrawal@avizva.com>
2020-08-30 11:39:56 +0530

    sync

--- pages/api/product/[id].js
+++
@@ -1,7 +0,0 @@
-export default async (req, res) => {
-    const {
-      query: { id },
-    } = req
-  
-    res.end(`Post: ${pid}`)
-}
--- pages/api/products.js
+++
@@ -1,9 +0,0 @@
-const Product = require( '../../backend/models/Product' );
-
-
-export default async (req, res) => {
-    
-    const products = await Product.find();
-  
-    res.end( products )
-}
