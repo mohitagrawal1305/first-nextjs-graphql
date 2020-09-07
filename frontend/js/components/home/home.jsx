2035fb0c2f473acbefa3a729058b5503f621296b
Mohit Agrawal <mohit.agrawal@avizva.com>
2020-09-07 23:36:30 +0530

    added banner

--- frontend/js/components/home/home.jsx
+++ frontend/js/components/home/home.jsx
@@ -2,10 +2,9 @@ import { search as Search } from '../search';
 import { ProductsList } from '../ProductsList';
 
 export const home = () => {
-
     return (
       <main className = 'home' >
-        <section className= 'home__main-section' >
+        <section className= 'home__main-section' style = { { backgroundImage: `url( https://shop.redq.now.sh/_next/static/images/cloths-bc740630f228713e4bb0de8a7bffce22.png )` } } >
           <h1 className= 'home__main-section__title' >Shop here</h1>
           <Search/>
         </section>
